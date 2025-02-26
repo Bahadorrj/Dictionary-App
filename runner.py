655b32ac4d18c58781d5f9c884aa75047e665cc6
Bahador Rousta <bahador.jorshary@gmail.com>
2025-02-26 23:53:37 +0330

    Remove style sheet assigning to the whole application

--- runner.py
+++ runner.py
@@ -4,13 +4,8 @@ from PyQt6.QtGui import QIcon
 from src.view import DictionaryApp
 from src.backend import resource_path
 
-MODE = "dark"  # or "light"
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    with open(resource_path(f"resources/{MODE}_mode.qss"), "r") as file:
-        style = file.read()
-        app.setStyleSheet(style)
     icon = QIcon(resource_path("resources/app.jpeg"))
     app.setWindowIcon(icon)
     window = DictionaryApp()
